eaf31531065c0c9d1b04ab4bc5232ddf0b934ce7
salmanakhtarno1 <salman.sallu449@gmail.com>
2020-12-26 09:46:13 +0500

    age finder
    it finds age of citizens

---
+++ main.js
@@ -0,0 +1,14 @@
+console.log("Old Citizen Finder")
+let age=79;
+if(age>=60){
+    console.log("you are Old citizen")
+}
+else if(age>35 || age<50){
+    console.log("you are senior citizen")
+}
+else if(age>=20 || age<=35){
+    console.log("you are young")
+}
+else{
+    console.log("you are child")
+}
